9139b35d184210eb2f6fe0a22deb3ce3e10b8dac
Craig Fox <craig@craigwfox.com>
2025-11-18 20:24:30 -0600

    cassido 2025-11-17

---
+++ cassido/2025-11-17/others.ts
@@ -0,0 +1,20 @@
+/*
+* Given a positive integer n,
+* write a function that returns an array containing all integers from 1 to n,
+* where each integer i appears exactly i times in the result.
+* For example, 3 should appear 3 times, 5 should appear 5 times, and so on.
+* The order of the integers in the output array does not matter.
+*
+* Example Output:
+* > repeatedIntegers(4)
+> [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+*/
+
+function repeatedIntegers(x: number) {
+  return Array.from({ length: x }).flatMap((y, index) => {
+    return Array.from({ length: index + 1 }).map(
+      y => index + 1
+    )
+  })
+}
+console.log(repeatedIntegers(4))
---
+++ cassido/2025-11-17/take2.ts
@@ -0,0 +1,19 @@
+/*
+* Given a positive integer n,
+* write a function that returns an array containing all integers from 1 to n,
+* where each integer i appears exactly i times in the result.
+* For example, 3 should appear 3 times, 5 should appear 5 times, and so on.
+* The order of the integers in the output array does not matter.
+*
+* Example Output:
+* > repeatedIntegers(4)
+> [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+*/
+
+function repeatedIntegers(x: number) {
+  const arry: number[] = []
+  for (let i = 1; i <= x; i++)
+    for (let p = 0; p < i; p++) arry.push(i)
+  return arry
+}
+repeatedIntegers(4)
